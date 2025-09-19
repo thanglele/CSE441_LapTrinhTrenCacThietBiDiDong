1969c754e6ca5daee5a156bcd8f4b64bf2e51655
Lê Thắng <lesythang2@gmail.com>
2025-09-19 10:42:05 +0700

    ReInit Project

--- android/app/src/main/kotlin/com/tlu/tempproject/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tlu.tempproject
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ mytlu/android/app/src/main/kotlin/com/etlu/mytlu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.etlu.mytlu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
