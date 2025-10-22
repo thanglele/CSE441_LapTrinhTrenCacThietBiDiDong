77d8fb5aca3f17bc57b30275a08c825a0a317c86
Lê Thắng <lesythang2@gmail.com>
2025-10-22 23:05:33 +0700

    Update CSDL record

---
+++ MyTLUSQL/SQLQuery6.sql
@@ -0,0 +1,6 @@
+﻿SELECT * FROM students as S
+inner join login as L on S.student_code = L.username
+inner join student_details as SL on SL.student_code = S.student_code
+inner join student_identification as SI on SI.student_code = S.student_code
+
+where S.full_name = N'Lê Văn Quân'
