3f9082c9d672cacc4a032543dc0af368822c29a8
AnhNgo127 <161997871+AnhNgo127@users.noreply.github.com>
2025-11-05 04:11:13 +0700

    Update EnsureUserIsAuthenticated.php

--- MyTLUWebsite/MyTLU_Frontend/app/Http/Middleware/EnsureUserIsAuthenticated.php
+++ MyTLUWebsite/MyTLU_Frontend/app/Http/Middleware/EnsureUserIsAuthenticated.php
@@ -3,27 +3,16 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session; // Import Session
+use Illuminate\Support\Facades\Auth;
 
 class EnsureUserIsAuthenticated
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
-        // Kiểm tra xem session 'auth_token' (đã lưu ở Controller) có tồn tại không
-        if (!Session::has('auth_token')) {
-            // Nếu không có, đá về trang login
-            return redirect()->route('login');
+        if (!Auth::check()) {
+            return redirect('/login');
         }
 
-        // Nếu có, cho phép request đi tiếp
         return $next($request);
     }
 }
