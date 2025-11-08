51eef1c425a7ebe8135e8ad65876f965fcacd865
Lê Thắng <lesythang2@gmail.com>
2025-11-09 02:34:38 +0700

    Final Commit, Publish Web with Docker

--- MyTLUWebsite/MyTLU_Frontend/vite.config.js
+++ MyTLUWebsite/MyTLU_Frontend/vite.config.js
@@ -12,4 +12,13 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        host: '0.0.0.0',      // lắng nghe mọi interface
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'remote.thanglele.cloud', // HMR sẽ gửi event về host này
+            port: 5173,
+        },
+    },
 });
